b8e816c0fe821f9185671da64d5a25314886e8f5
Rubi <60947134+ruvolpe@users.noreply.github.com>
2022-03-17 19:55:49 +0000

    Add files via upload

---
+++ Fatorial.java
@@ -0,0 +1,9 @@
+public class Fatorial {
+	public static void main(String[] args) {
+		int fatorial = 1;
+		for (int n = 1; n < 11; n++) {
+			fatorial *= n;
+			System.out.println(fatorial);
+		}
+	}
+}
