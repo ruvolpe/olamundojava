847ce85b2e297a284fff35a2976b1aa7bd542a54
Rubi <60947134+ruvolpe@users.noreply.github.com>
2022-03-17 19:54:31 +0000

    Add files via upload

---
+++ SintaxeVariaveisFluxo.java
@@ -0,0 +1,16 @@
+public class SintaxeVariaveisFluxo {
+    public static void main (String[] args){
+        System.out.println("Ola novo teste");
+
+        int idade;
+        idade = 37;
+
+        System.out.println(idade);
+
+        idade = 30 + 10;
+        System.out.println(idade);
+        idade = 5 * 7 + 2;
+        System.out.println(idade);
+        System.out.println("A idade é " + idade);
+    }
+}
